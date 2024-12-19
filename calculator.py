3e91c138831003387c431996ae0344535cd4dab4
Shafaiz Ansari <103962343+shafez-ansari@users.noreply.github.com>
2024-12-19 14:11:06 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,12 +7,12 @@ num2 = 6.3
 sum = num1 + num2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 a = int(1)
 b = int(2.8)
 c = int("3")
 print(a)
 print(b)
-print(c
+print(c)
 
